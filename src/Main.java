803a26c92540a6a0fafb553bdbbb5f7138283f27
enesyasa <108360836+enesyasa@users.noreply.github.com>
2024-02-21 12:04:16 +0300

    yorum satırları eklendi

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
         int a,b,c,sonuc;
+
         Scanner inp=new Scanner(System.in);
+
+       //kullanıdan girdiler alınması sağlandı
         System.out.print("a sayısını girin:");
         a=inp.nextInt();
 
@@ -13,6 +16,7 @@ public class Main {
         System.out.print("c sayısını girin:");
         c=inp.nextInt();
 
+    //sonuc ekrana yazdırıldı
         sonuc=a+b*c-b;
         System.out.print("sonuç: "+sonuc);
 
